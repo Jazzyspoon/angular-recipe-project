08787ec18999c2e2e6e281206442ae782e62fac9
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-10-26 12:14:24 -0600

    component basics set in place

--- src/app/recipes-component/recipe-list/recipe-list.component.ts
+++ src/app/recipes-component/recipe-list/recipe-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'TEST RECIPE',
+      'TEST DESCRIPTION',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/slow-cooker-spanish-chicken-4b787a1.jpg?quality=90&webp=true&resize=375,341'
+    ),
+    new Recipe(
+      'Test recipe',
+      'Test Description',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/slow-cooker-spanish-chicken-4b787a1.jpg?quality=90&webp=true&resize=375,341'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shoppinglist-component/shoppinglist-component.component.ts
+++ src/app/shoppinglist-component/shoppinglist-component.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shoppinglist-component',
   templateUrl: './shoppinglist-component.component.html',
-  styleUrls: ['./shoppinglist-component.component.css']
+  styleUrls: ['./shoppinglist-component.component.css'],
 })
 export class ShoppinglistComponentComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
