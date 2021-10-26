6afb23fdd6265ae7e7242252969fe6e00ece230e
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-10-26 10:54:59 -0600

    header completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { ShoppinglistComponentComponent } from './shoppinglist-component/shoppinglist-component.component';
-
 import { FooterComponentComponent } from './footer-component/footer-component.component';
 import { RecipesComponentComponent } from './recipes-component/recipes-component.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeListComponent } from './recipes-component/recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipes-component/recipe-details/recipe-details.component';
-
+import { ShoppingEditComponent } from './shoppinglist-component/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes-component/recipe-list/recipe-item/recipe-item.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
     ShoppinglistComponentComponent,
-
     FooterComponentComponent,
     RecipesComponentComponent,
     RecipeListComponent,
     RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
