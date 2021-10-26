f88b27cfd68c4958b023c815e63266a5aa95dcf0
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-10-26 09:28:02 -0600

    building the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { ShoppinglistComponentComponent } from './shoppinglist-component/shoppinglist-component.component';
+
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { RecipesComponentComponent } from './recipes-component/recipes-component.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './recipes-component/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    ShoppinglistComponentComponent,
+
+    FooterComponentComponent,
+    RecipesComponentComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
